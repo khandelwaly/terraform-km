2683a9f57a9fae18e112ad8804f651377cca9914
khandelwaly <yash.khandelwal.inbox@gmail.com>
2021-06-19 12:14:21 +0530

    terraform changes applied

--- spartans/yash/main.tf
+++ spartans/yash/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr1" {
-  name                 = "km-terraform-session-1"
+  name                 = "spartans-yash-ecr"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
